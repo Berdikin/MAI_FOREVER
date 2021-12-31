5d88e3fd1b32f2e454bda3f0bfb5ee79346ec453
Timofey <54998271+Berdikin@users.noreply.github.com>
2021-12-31 07:50:20 +0300

    Delete FP/lab1/lab2 directory

--- FP/lab1/lab2/lab2.lisp
+++
@@ -1,6 +0,0 @@
-(defun delete_end(list)                      
-  (cond ((null (cdr list)) 
-     nil)                        
-    (t 
-     (cons (car list) 
-           (delete_end (cdr list))))))
